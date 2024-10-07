9a3a7ee713c41b8a8fc7776b6ac7437ff3a3243d
zczqas <limbusanchar76@gmail.com>
2024-10-07 17:14:26 +0545

    fix

---
+++ pkg/database/db.go
@@ -0,0 +1,55 @@
+package database
+
+import (
+	"awesomeProject/internal/models"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Config struct {
+	Username string
+	Password string
+	Host     string
+	Port     string
+	DBName   string
+}
+
+func NewConfig() Config {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Failed to load env file: %v", err)
+	}
+
+	return Config{
+		Username: os.Getenv("DB_USERNAME"),
+		Password: os.Getenv("DB_PASSWORD"),
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		DBName:   os.Getenv("DB_NAME"),
+	}
+}
+
+func Connect(config Config) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		config.Username,
+		config.Password,
+		config.Host,
+		config.Port,
+		config.DBName,
+	)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %w", err)
+	}
+
+	if err := db.AutoMigrate(&models.User{}); err != nil {
+		return nil, fmt.Errorf("failed to perform auto migration: %w", err)
+	}
+
+	return db, nil
+}
